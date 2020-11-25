7f4cc4dbe7380878eaca64108a75fff72c85e298
Joseph <24699965@qq.com>
2020-11-25 11:37:15 +0800

    start the 11 chapter read

---
+++ Advanced JavaScript programming/10_函数/10.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+// 调用对象使用this 并且传参
+function sum(a, b) {
+    console.log(a + b);
+}
+
+function test(a, b) {
+    console.log(a);
+    console.log(b);
+    sum.apply(this, [a, b]);
+}
+
+test(1, 2);
